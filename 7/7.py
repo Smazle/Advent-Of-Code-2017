bf07e932e72e6f02ad38705de4bf8cbe8acc2b93
Smazle <August.vinkel@gmail.com>
2018-04-03 20:50:45 +0200

    Made up to 7.2

---
+++ 7/7.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+weights = {}
+relations = {}
+
+
+main = "gmcrj"
+
+
+with open("./input") as f:
+    for line in f:
+        line = line.rstrip()
+        line = line.replace(",", "")
+        line = line.split(" ") 
+
+        name = line[0]
+        weights[name] = int(line[1].strip("()"))
+
+        if len(line) > 2:
+            relations[name] = line[3:]
+        else:
+            relations[name] = []
+
+
+def weight(node, w):
+    
+    
+
